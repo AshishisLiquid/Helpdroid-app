f01b8e4e6832c7f4bfbe77048b549e502e52a0bf
unknown <ashishpise111@gmail.com>
2020-03-02 22:40:26 +0530

    Message Api Completed

--- app/src/main/java/co/syntags/helpdroid/MainActivity.java
+++ app/src/main/java/co/syntags/helpdroid/MainActivity.java
@@ -1,24 +1,97 @@
 package co.syntags.helpdroid;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    Button panicBtn, sendMessageBtn, viewMapBtn;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        panicBtn = findViewById(R.id.panicBtn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        sendMessageBtn = findViewById(R.id.sendMessageBtn);
+        viewMapBtn = findViewById(R.id.mapBtn);
+        panicBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "100", null));
+                startActivity(intent);
+            }
+        });
+        if (firebaseAuth.getCurrentUser() != null) {
+            sendMessageBtn.setVisibility(View.VISIBLE);
+        } else {
+            sendMessageBtn.setVisibility(View.GONE);
+        }
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase.getInstance().getReference()
+                        .child(firebaseAuth.getCurrentUser().getUid())
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                String sms = "Help";
+                                ArrayList<String> numbers = new ArrayList<>(Arrays.asList(dataSnapshot.child("Family Member 1").getValue(String.class), dataSnapshot.child("Family Member 2").getValue(String.class), dataSnapshot.child("Family Member 3").getValue(String.class)));
+                                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
+                                        != PackageManager.PERMISSION_GRANTED) {
+                                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 1);
+                                } else {
+                                    for (int i = 0; i < numbers.size(); i++) {
+                                        try {
+
+                                            SmsManager smsManager = SmsManager.getDefault();
+                                            smsManager.sendTextMessage(numbers.get(i), null, sms, null, null);
+                                            Log.e("sent", "success");
+
+                                        } catch (Exception e) {
+                                            Log.e("sent", "failed");
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+            }
+        });
     }
 
     @Override
@@ -31,6 +104,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.profileIcon:
+                if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
+                } else {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
                 break;
             default:
                 return super.onOptionsItemSelected(item);
@@ -38,12 +116,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onStart() {
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
-            finish();
-        }
-        super.onStart();
-    }
 }
